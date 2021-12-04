133b4c2b490d6359763c8bb60d8c8ff370b8ced5
Vineet Reddy <vineetur@gmail.com>
2021-12-04 13:52:07 +1100

    Update Tests for MathUtils Added 2 More Test for Coverage of Negative Scenarios

--- ConsolePrint/src/test/java/com/vineet/ConsolePrint/MathUtilsTest.java
+++ ConsolePrint/src/test/java/com/vineet/ConsolePrint/MathUtilsTest.java
@@ -11,11 +11,31 @@ class MathUtilsTest {
 		
 		MathUtils mutils = new MathUtils();
 		int result = mutils.add(3, 5);
-		System.out.println(result);
-		assertEquals(8, result);
-		//System.out.println("Test Additon of 3, 5 => 8 " + result);
-		//fail("Not yet Implemented");
+		// System.out.println(result);
+		System.out.println("Test Additon of 3, 5 => Expected > 8 :: Actual > " + result);
+		assertEquals(8, result);		
 		
 	}
 
+	@Test
+	void testAdd_02() {
+		
+		MathUtils mutils = new MathUtils();
+		int result = mutils.add(4, 5);
+		// System.out.println(result);
+		System.out.println("Test Additon of 4, 5 => Expected > 8 :: Actual > " + result);
+		assertEquals(8, result);		
+		
+	}
+	
+	@Test
+	void testAdd_03() {
+		
+		MathUtils mutils = new MathUtils();
+		int result = mutils.add(3, -5);
+		// System.out.println(result);
+		System.out.println("Test Additon of 3, -5 => Expected > -2 :: Actual > " + result);
+		assertEquals(-2, result);		
+		
+	}
 }
